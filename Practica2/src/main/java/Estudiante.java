b69faf82888f27544e0cb505b604839c5f0f6631
Ernesto Rodríguez <ernestorod27@gmail.com>
2016-05-29 15:34:38 -0400

    Freemarker Ready

---
+++ Practica2/src/main/java/Estudiante.java
@@ -0,0 +1,46 @@
+public class Estudiante{
+
+    private String matricula;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+
+    public Estudiante(String matricula, String nombre, String apellidos, String telefono) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ Practica2/src/main/java/Main.java
@@ -0,0 +1,143 @@
+/**
+ * Created by Ernesto on 5/28/2016.
+ */
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        get("/hello", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            // The hello.ftl file is located in directory:
+            // src/test/resources/spark/template/freemarker
+            return new ModelAndView(attributes, "hello.ftl");
+        }, new FreeMarkerEngine());
+
+    }
+
+}
+/*
+import static spark.Spark.*;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+        Configuration freeMarkerConfiguration = new Configuration();
+        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(BlogService.class, "/"));
+        freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+
+        get("/posts", (request, response) -> {
+            if (shouldReturnHtml(request)) {
+                response.status(200);
+                response.type("text/html");
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("posts", model.getAllPosts());
+                return freeMarkerEngine.render(new ModelAndView(attributes, "posts.ftl"));
+            } else {
+                response.status(200);
+                response.type("application/json");
+                return dataToJson(model.getAllPosts());
+            }
+        });
+        Map attributes = new HashMap<>();
+        attributes.put("posts", model.getAllPosts());
+
+        return freeMarkerEngine.render(new ModelAndView(attributes, "posts.ftl"));
+    }
+}*/
+/*---------------------------------------------------------------------------------------------------------*/
+/*
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.Version;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    // 1. Configure FreeMarker
+    //
+    // You should do this ONLY ONCE, when your application starts,
+    // then reuse the same Configuration object elsewhere.
+
+    Configuration cfg = new Configuration();
+
+    // Where do we load the templates from:
+    cfg.setClassForTemplateLoading(Main.class, "templates");
+
+    // Some other recommended settings:
+    cfg.setIncompatibleImprovements(new Version(2, 3, 20));
+    cfg.setDefaultEncoding("UTF-8");
+    cfg.setLocale(Locale.US);
+    cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+    // 2. Proccess template(s)
+    //
+    // You will do this for several times in typical applications.
+
+    // 2.1. Prepare the template input:
+
+    Map<String, Object> input = new HashMap<String, Object>();
+
+    input.put("title", "Vogella example");
+
+    input.put("exampleObject", new Estudiante("12", "E","R","809"));
+
+    List<Estudiante> systems = new ArrayList<Estudiante>();
+    systems.add(new Estudiante("20120201", "Ernesto","Rodríguez","809-471-3978"));
+      systems.add(new Estudiante("20120844", "Eduardo","Veras","829-456-2379"));
+
+    input.put("systems", systems);
+
+    // 2.2. Get the template
+
+    Template template = cfg.getTemplate("helloworld.ftl");
+
+    // 2.3. Generate the output
+
+    // Write output to the console
+    Writer consoleWriter = new OutputStreamWriter(System.out);
+    template.process(input, consoleWriter);
+
+    // For the sake of example, also write output into a file:
+    Writer fileWriter = new FileWriter(new File("output.html"));
+    try {
+      template.process(input, fileWriter);
+    } finally {
+      fileWriter.close();
+    }
+
+  }
+}
+*/
